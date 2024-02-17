39f8cdd678900617a4bcf77eec715a9a2d9cf937
Suyog Bhandari <079bct091.suyog@pcampus.edu.np>
2024-02-17 23:45:52 +0545

    this is my config of nvim

---
+++ lua/nvimConfig/packer.lua
@@ -0,0 +1,72 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+--
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	--here are some themes i use
+
+	use { 'projekt0n/github-nvim-theme' }
+
+use { "ellisonleao/gruvbox.nvim" }
+
+use "bluz71/vim-nightfly-colors"
+
+--this is for nerdtree
+use {'preservim/nerdtree'}
+
+--this is for icons in nerd tree
+
+use {'ryanoasis/vim-devicons'}
+
+--this is for vim-airline
+use {'vim-airline/vim-airline'}
+use{'vim-airline/vim-airline-themes'}
+
+--for treesitter
+
+use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use ('nvim-treesitter/playground')
+--for harpoon
+use('ThePrimeagen/harpoon')
+--for undo tree
+use('mbbill/undotree')
+--for git apparently
+use('tpope/vim-fugitive')
+
+
+use('BurntSushi/ripgrep')
+--this apparently is for lsp
+
+use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+}
+
+use{'hrsh7th/cmp-nvim-lsp'}
+use{'hrsh7th/cmp-buffer'}
+use{'hrsh7th/cmp-path'}
+use{'hrsh7th/cmp-cmdline'}
+use{'hrsh7th/nvim-cmp'}
+
+
+--for telescope ui again(this time it gives nice little window for code actions)
+
+use('nvim-telescope/telescope-ui-select.nvim')
+
+--for code snippets
+
+use{'L3MON4D3/LuaSnip'}
+use{'saadparwaiz1/cmp_luasnip'}
+use{'rafamadriz/friendly-snippets'}
+end)
